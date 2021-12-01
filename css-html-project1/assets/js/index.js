d57edf6bd5e935c5119cd7c28ebde71fbc14465b
LAPTOP-J8JDA84E\mahsa <mahsamoradi611.mm@gmail.com>
2021-12-01 12:54:30 +0100

    responsive

--- css-html-project1/assets/js/index.js
+++ css-html-project1/assets/js/index.js
@@ -1,6 +1,9 @@
 function myFunc(){
     var x = document.getElementById("mynav");
     if(x.className === "nav"){
-        x.className += " responsive";
+        x.className += " responsive"; //space is so important
+    }
+    else{
+        x.className="nav";
     }
 }
