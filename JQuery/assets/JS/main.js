928934fcd9fddbe25331af03ac4ffe3cf42b8946
mahsa <mahsamoradi611.mm@gmail.com>
2021-12-20 11:59:12 +0100

    jquery events

--- JQuery/assets/JS/main.js
+++ JQuery/assets/JS/main.js
@@ -1,18 +1,41 @@
 // $(document).ready(function(){
-//      $('.test').hide(1000);  
+//      $('.test').hide(1000);
 // })
 
-$(function(){
-    $('.test').show();
+$(function () {
+  $("div.test").show();
 });
 
-
-$(function(){
-$('.m span').hide();
+$(function () {
+  $(".m span").hide();
 });
 
-$(document).ready(function(){
-$(".click").mouseEnter(function(){
-$(".click").hide();
+$(document).ready(function () {
+  // $(".click").dblclick(function(){
+  // $(".click").mouseleave(function(){
+  // $(".click").mouseenter(function(){
+  // $(".click").mousedown(function(){
+  $(".click").mouseup(function () {
+    $(".click").hide();
+  });
 });
+
+$(document).ready(function () {
+  $(".m").on("click", function () {
+    $("div").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("p.click1").on({
+    click: function () {
+      $("p.click1").css("color", "red");
+    },
+    mouseleave:function(){
+        $("p.click1").css("color","blue");
+    }
+    // :function(){
+    //     $("click1").css("","")
+    // }
+  });
 });
