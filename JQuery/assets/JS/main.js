62a26c8c1955fdf15210706d799e738aeff9a8cc
mahsa <mahsamoradi611.mm@gmail.com>
2021-12-22 21:30:44 +0100

    test

--- JQuery/assets/JS/main.js
+++ JQuery/assets/JS/main.js
@@ -31,11 +31,55 @@ $(document).ready(function () {
     click: function () {
       $("p.click1").css("color", "red");
     },
-    mouseleave:function(){
-        $("p.click1").css("color","blue");
-    }
+    mouseleave: function () {
+      $("p.click1").css("color", "blue");
+    },
     // :function(){
     //     $("click1").css("","")
     // }
   });
 });
+
+$(document).ready(function () {
+  //   $(".c1").click(function () {
+  //     $(".test1").fadeOut("60s");
+  //   });
+  $(".c1").hover(function () {
+    $(".test1").slideUp("60s");
+  });
+  //   *****************************
+  //   $(".c2").click(function () {
+  //     $(".test1").fadeIn("slow");
+  //   });
+  $(".c2").hover(function () {
+    $(".test1").slideDown("60s");
+  });
+  // *****************************
+
+  //   $(".c3").click(function () {
+  //     $(".test1").fadeToggle("80s");
+  //   });
+  $(".c3").click(function () {
+    $(".test1").slideToggle("80s");
+  });
+  //   ********************************
+  $(".c4").click(function () {
+    $(".test1").fadeTo("slow", 0.7);
+  });
+});
+// Animation*********************
+$(document).ready(function () {
+  $(".b1").click(function () {
+    
+    $(".testb1").animate({
+    width:"150px",
+    // height:"300px" ,
+    height:"toggle" ,
+    bottom:"100px" , 
+    left:"100px" , 
+    opacity:"0.5", 
+    borderRadius:"80px" 
+    },5000);
+  });
+});
+
