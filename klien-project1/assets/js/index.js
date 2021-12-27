a9ad67b508359be0a78b1c42da19f6763456a201
mahsa <mahsamoradi611.mm@gmail.com>
2021-12-27 17:42:35 +0100

    scroll to top

--- klien-project1/assets/js/index.js
+++ klien-project1/assets/js/index.js
@@ -9,13 +9,36 @@
 // });
 // });
 
-$(document).ready(function(){
-$(".accordion > li > div").click(function(){
+$(document).ready(function () {
+  $(".accordion > li > div").click(function () {
     // display:'block'
-if($(this).next().is(":visible")== false){
-    $(".accordion ul").slideUp();
-}
-$(this).next().slideToggle();
-});
+    if ($(this).next().is(":visible") == false) {
+      $(".accordion ul").slideUp();
+    }
+    $(this).next().slideToggle();
+  });
 });
 // scroll to top**********************
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 200) {
+    // $('.scroll-to-top').fadeIn('slow');
+    $(".scroll-to-top").css({
+      bottom: "20px",
+    });
+  } else {
+    // $('.scroll-to-top').fadeOut('slow');
+    $(".scroll-to-top").fadeOutcss({
+      bottom: "-60px",
+    });
+  }
+});
+
+$(".scroll-to-top").click(function (e) {
+  e.preventDefault();
+  $("body,html").animate(
+    {
+      scrollTop: 0,
+    },
+    1000
+  );
+});
