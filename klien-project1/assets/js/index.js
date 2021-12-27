a3c1998657860912d6553d224c9f7754c5b5dd6a
mahsa <mahsamoradi611.mm@gmail.com>
2021-12-27 15:23:20 +0100

    accordion menu

--- klien-project1/assets/js/index.js
+++ klien-project1/assets/js/index.js
@@ -1,7 +1,10 @@
 var j = $noConfict();
 j(document).ready(function(){
-j(".accordion >li >div").click(function(){
+j(".accordion > li > div").click(function(){
     // display:'block'
-if(j(this).next().is(:visible));
+if(j(this).next().is(":visible")== false){
+    j(".accordion ul").slideUp();
+}
+j(this).next().slideToggle();
 });
 });
