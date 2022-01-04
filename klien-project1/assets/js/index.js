cba18432353e1c78575ffb0614a2f5f6d926fa47
mahsa <mahsamoradi611.mm@gmail.com>
2022-01-04 23:03:27 +0100

    Add FORM

--- klien-project1/assets/js/index.js
+++ klien-project1/assets/js/index.js
@@ -68,9 +68,11 @@ $(document).ready(function () {
 $(document).ready(function () {
   $("#add").click(function (e) {
     e.preventDefault();
-    $(".input-group").after("<div class='input-group'><label for='input' >Geschidenis</label><input type='text' id='input'  placeholder='Add Informatie...'/></div>");
+    $("#add").before("<div class='input-group'><label for='input' >Geschidenis</label><input type='text' id='input'  placeholder='Add Informatie...'/></div>");
+    // .input-group :khodesh ro ijad bokon
+    // #add: ye onsor digei ro ijad kon
   });
 });
 //  TIPS1: "append va prepend"= az Daron ezafeh mishavand 
-// "befor va after": az Biron ezafeh mishavand
-// TIPS2: chikar konim ke 1 ya 2 ya 3 ta INPUT ezafeh shavad?  bayad vabastegi Anasor ro bardarim  
+//  "befor va after": az Biron ezafeh mishavand
+//  TIPS2: chikar konim ke 1 ya 2 ya 3 ta INPUT ezafeh shavad?  bayad vabastegi Anasor ro bardarim YANI begim "#add" be jaye ".input-group" .
