1ad456a2bcdc81c6a02aa57bda6aeb113d2e8b4d
mahsa <mahsamoradi611.mm@gmail.com>
2022-01-05 14:22:07 +0100

    sweet alert

---
+++ plagins/assets/js/index.js
@@ -0,0 +1,11 @@
+// var j = $noConfict();
+// j(document).ready(function(){
+// j(".accordion > li > div").click(function(){
+//     // display:'block'
+// if(j(this).next().is(":visible")== false){
+//     j(".accordion ul").slideUp();
+// }
+// j(this).next().slideToggle();
+// });
+// });
+
