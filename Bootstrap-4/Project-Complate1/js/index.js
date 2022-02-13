5c53e8bf92fdf6c598a4dde427505b3ae76abb27
mahsa <mahsamoradi611.mm@gmail.com>
2022-02-13 23:15:04 +0100

    count

--- Bootstrap-4/Project-Complate1/js/index.js
+++ Bootstrap-4/Project-Complate1/js/index.js
@@ -9,10 +9,10 @@ var X = setInterval(function(){
     var distance = countDownDate - now;
 
 
-var  day = Math.floor(distance / (1000 * 60 * 60* 24));
-var  hours = Math.floor(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var  mintutes = Math.floor(distance % (1000 * 60 * 60)) / (1000 * 60));
-var  seconds = Math.floor(distance % (1000 * 60 )) / 1000);
+var  days = Math.floor(distance / (1000 * 60 * 60* 24));
+var  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var  mintutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var  seconds = Math.floor((distance % (1000 * 60 )) / 1000);
 
 Document.getElenentById('demo').innerHTML = '<span id = "day">'+ day + '</span>' + '<span id = "hours">'+ hours + '</span>'  +  '<span id = "mintutes">'+ mintutes + '</span>' + '<span id = "mintutes">'+ seconds + '</span>'; 
 
