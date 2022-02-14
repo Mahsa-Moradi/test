a8ace16d93a663499fe691bb49be55a0e9cf7c62
mahsa <mahsamoradi611.mm@gmail.com>
2022-02-14 01:05:13 +0100

    ordering

--- Bootstrap-4/Project-Complate1/js/index.js
+++ Bootstrap-4/Project-Complate1/js/index.js
@@ -2,25 +2,25 @@
 var countDownDate = new Date('May 5 , 2022 15:37:25').getTime();
 
 var X = setInterval(function(){
-    var now = new Date() .getTime();
+    var now = new Date().getTime();
 
     var distance = countDownDate - now;
 
 
-var  days = Math.floor(distance / (1000 * 60 * 60* 24));
+var  days = Math.floor(distance / (1000 * 60 * 60 * 24));
 var  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var  mintutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var  seconds = Math.floor((distance % (1000 * 60 )) / 1000);
 
-Document.getElenentById('demo').innerHTML = '<span id = "day">'+ day + '</span>' + '<span id = "hours">'+ hours + '</span>'  +  '<span id = "mintutes">'+ mintutes + '</span>' + '<span id = "seconds">'+ seconds + '</span>'; 
+document.getElementById("demo").innerHTML = '<span id = "day">'+ days + '</span>' + '<span id = "hours">'+ hours + '</span>'  +  '<span id = "minutes">'+ minutes + '</span>' + '<span id = "seconds">'+ seconds + '</span>'; 
 
 
 if (distance < 0) {
-    clearInterval(X);
-    Document.getElenentById('demo').innerHTML = Finish
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = Finish
 }
 
-} , 1000 );
+} , 1000);
 
 
 
