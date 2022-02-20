aa18c5442c9a70c22d0725749789f5942b57d923
mahsa <mahsamoradi611.mm@gmail.com>
2022-02-20 18:50:01 +0100

    gulp

---
+++ GULPAPP/gulpfile.js
@@ -0,0 +1,5 @@
+const gulp= require('gulp');
+// gulp.task - Define tasks /(moshakhas kardan yek vazifeh)/
+// gulp.src - Point to files to use
+// gulp.dest (dist folder)- -Points to folder to output
+// gulp.watch  -Watch file and folders for changes   /(file haye Src  ke hey taghir mikoneh ro NAGAH mikoneh)/
