1697caa8c4861e678cafe142839463e06ee878db
mahsa <mahsamoradi611.mm@gmail.com>
2022-04-21 12:05:19 +0200

    boolean

--- javascript/variable/js/main.js
+++ javascript/variable/js/main.js
@@ -1,4 +1,7 @@
 // variable****************************
+
+
+// ******      Number   ********
 var a = 5;
 // console.log(a);
 // .........................
@@ -16,25 +19,44 @@ var e =a / b;
 
 var firstName = "Mahsa";
 // console.log(firstName);
-// .............................
+
+
+
+// ******      String   ********
 var name0 = "mahsa 'moradi'";
 // console.log(name0);
 // ..............
 var name0 = 'mehrdad \'moradi\'';
-console.log(name0);
+// console.log(name0);
 // .............................
+var number1 ="mahyar 'moradi'";
+// console.log(number1.length);
 
+// ******     boolean   ********
+var f =false;
+// console.log(f);
+// ...........................
+var g = true;
+// console.log(g);
+// ............................
+var h = (10<8);
+console.log(h);
+// ............................
+var i = (10>8);
+console.log(i);
+
+// ............................
 
 
 
 // OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
-var number1 = 20;
-number1 = 50;
-// console.log(number1);
-// OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
-// const number2 = 60;
-// number2 = 70 ;
+var number2 = 20;
+number2 = 50;
 // console.log(number2);
+// OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+// const number3 = 60;
+// number3 = 70 ;
+// console.log(number3);
 // OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 // constant******************************
 
