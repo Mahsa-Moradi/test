9e7b0e322375d22d32ee113d924f8b7e158694a2
mahsa <mahsamoradi611.mm@gmail.com>
2022-04-21 19:13:58 +0200

    ||

--- javascript/if/js/main.js
+++ javascript/if/js/main.js
@@ -1,15 +1,22 @@
-// ***************  if  ***************
+// ***************  if  ***************************
 
 var gender = "male";
 // var gender = "female";
-var age = 18;
-if( gender == "male" &&  age>=15 ){
+var age = 28;
+// .............. && ............
+
+if( gender == "male" &&  age >=18 ){
     console.log(" welcome male");
 }
 else{
     console.log("welcome female");
 }
-// console.log('welcome');
-// var w=10;
-// var e=+40;
-// console.log(e);
+// .............. ||  ............
+
+if( gender == "female" ||  age >=18 ){
+    console.log(" welcome dear");
+}
+else{
+    console.log("error");
+}
+
