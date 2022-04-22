3523ce223feb0790cc3fc81dd1d500581b9f9fb8
mahsa <mahsamoradi611.mm@gmail.com>
2022-04-22 16:03:46 +0200

    loop-for.mult 1 to 5

--- javascript/loop/for/js/main.js
+++ javascript/loop/for/js/main.js
@@ -1,14 +1,18 @@
 // ********************************** for
 // ................ show number 1 to 5
-for(i=1 ; i<=5 ; i++){
-    // console.log( i);
+for (var i = 1; i <= 5; i++) {
+console.log(i);
 }
-// ................ show number 5 to 1
-for(j=5 ; j>=5 ; j++){
-    console.log( j );
 
+
+// ................ show number 5 to 1
+for (var j = 5; j >= 5; j++) {
+  console.log(j);  
 }
-var sum = 1;
-for(k=1 ; k<=5 ; k++){
-sum *= k;
+
+// ....................... mult 1 to 5
+var sum = 1 , k;
+for ( k = 1; k <= 5; k++) {
+  sum *= k;
 }
+console.log(sum);
