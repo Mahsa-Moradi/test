461243a859211d91ba1082d9245475d30541eb21
mahsa <mahsamoradi611.mm@gmail.com>
2022-04-22 12:02:11 +0200

    mode 7

--- javascript/loop/while/js/main.js
+++ javascript/loop/while/js/main.js
@@ -3,3 +3,17 @@ while (a <= 15) {
   console.log("Mahsa");
   a++;
 }
+// .......................
+var b = 1;
+while(b<=5){
+    console.log(b);
+    b++;
+}
+// ....................... mode 7
+var c = 10;
+while(c<=35){
+    if(c % 7 ==0){
+        console.log(c);
+    }
+    c++;
+}
