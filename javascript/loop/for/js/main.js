67abe133c6357c76255a7cd33622cde4b5b3ea12
mahsa <mahsamoradi611.mm@gmail.com>
2022-04-22 15:51:16 +0200

    loop-for

---
+++ javascript/loop/for/js/main.js
@@ -0,0 +1,14 @@
+// ********************************** for
+// ................ show number 1 to 5
+for(i=1 ; i<=5 ; i++){
+    // console.log( i);
+}
+// ................ show number 5 to 1
+for(j=5 ; j>=5 ; j++){
+    console.log( j );
+
+}
+var sum = 1;
+for(k=1 ; k<=5 ; k++){
+sum *= k;
+}
