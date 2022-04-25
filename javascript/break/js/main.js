a8423b7c0ee6e5c1717ec35b1ef5a46f894aca37
mahsa <mahsamoradi611.mm@gmail.com>
2022-04-25 12:53:49 +0200

    Continue

--- javascript/break/js/main.js
+++ javascript/break/js/main.js
@@ -1,58 +1,9 @@
-// *************************************   Conversion
+// *************************************   break
 // ........................... string to number
-console.log(Number('3.14'));
-// .....................
-console.log(Number(''));
-// .....................
-console.log(Number('  '));
-// .....................
-console.log(Number('vahid123'));
-// .....................
-console.log(Number('vahid 123'));
-// .....................
-console.log(Number('748 123'));
-// .....................
-var a='vahid';
-console.log(Number(a));
-// .....................
-var a='33';
-console.log(Number(a));
-4// .....................
-var a='33';
-console.log(Number(a));
-// .....................
-var a='33';
-console.log(Number(a));
-// .....................
-var a='33';
-console.log(Number(a));
-// ........................... number to  string 
-console.log(String(250));
-// .....................
-console.log(String(3.14));
-// .....................
-console.log(String(250 + 25));
-// ........................... Boolean  to  number
-console.log(Number(true));
-// .....................
-console.log(Number(false));
-// ........................... number  to  Boolean
-console.log(Boolean(250));
-console.log(Boolean(0));
-console.log(Boolean(1));
-console.log(Boolean(NaN));
-console.log(Boolean(null));
-console.log(Boolean(10 / 'hello'));
-console.log(Boolean(undefined));
-console.log(Boolean(""));
-// .......................... string  to  Boolean
-console.log(Boolean('0'));
-console.log(Boolean('1'));
-console.log(Boolean('milad'));
-console.log(Boolean('  '));
-console.log(Boolean(''));
-// .....................
-
-// .....................
-
+for( i=1 ; i<=10 ;i++){
+    console.log('Mehrdad');
+    if(i==5){
+        break;
+    }
+}
 // .....................
---
+++ javascript/continue/js/main.js
@@ -0,0 +1,17 @@
+// *************************   Continue
+var sum =0 , i=1 ;
+for( i=1 ; i<=10 ; i++){
+    sum += i;
+    if(i==6 ){
+        continue;
+    }
+    console.log('sum');
+}
+// .........................
+
+for(j=1 ; j<= 10 ; j++){
+    if(j%2==0){
+        continue;
+    }
+    console.log(j);
+}
