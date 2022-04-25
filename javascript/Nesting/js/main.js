0714176488761b718b12c454a021babb95f075e5
mahsa <mahsamoradi611.mm@gmail.com>
2022-04-25 19:57:21 +0200

    Nesting

--- javascript/Nesting/js/main.js
+++ javascript/Nesting/js/main.js
@@ -11,10 +11,28 @@ for( i=1 ; i<=2 ; i++){
 
 var str = '';
 for(var k=1 ; k<=2 ; k++){
+    console.log('str');
     for( var l=1 ; l<=3 ; l++){
-        // str+='*';
-        str = str + '*' ;
+        str+='*';
+        // str = str + '*' ;
+        console.log('str');
     }
     // str=+'\n';
     str = str + '\n' ;
+    
+}
+// .........................
+
+var str1 = '';
+for(var m=1 ; m<=5 ; m++){
+    console.log('str1');
+
+    for( var n=1 ; n<=m ; n++){
+        // str+='*';
+        str1 = str1 + '*' ;
+        console.log('str1');
+
+    }
+    // str=+'\n';
+    str1 = str1 + '\n' ;
 }
