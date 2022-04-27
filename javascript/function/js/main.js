ac41c7c0a0521abb5d3f00a07f89cf30f6c103fe
mahsa <mahsamoradi611.mm@gmail.com>
2022-04-27 12:51:35 +0200

    function expression add 2 number

--- javascript/function/js/main.js
+++ javascript/function/js/main.js
@@ -13,12 +13,13 @@ for(i=0;i<=addEventListener.length;i++){
 }
 console.log(a);
 // ............................. ********* function declaration
+// 1a ........................
 // inputs (num1,num2)
 function functionName(inputs){
     // code
 
 }
-// ........................
+// 2a ........................
 function showBio(){
     console.log('First name : ********* Mahsa');
     console.log('Last name : Moradi');
@@ -28,19 +29,26 @@ function showBio(){
 showBio();
 showBio();
 showBio();
-// ........................ add 2 number
+// 3a ........................ add 2 number
 function addnumbers(num1 , num2){
     console.log(num1 + num2);
 }
 addnumbers(5,200);
+addnumbers(5,10);
 // ............................. ********* function expression
+// 1b ........................
 var name = function(){
     // code
 }
-// ........................
+// 2b ........................
 var names = function(){
     console.log('First name : ....... Mehrdad');
     console.log('Last name : Moradi');
     console.log('Age : 34');
 }
 names();
+// 3b ........................ add 2 number
+var numbers1=function(num1,num2){
+    console.log(num1+num2);
+}
+numbers1(50,14);
