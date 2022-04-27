66f59f0adeb3b5f31e4c0a34feefb7e315828922
mahsa <mahsamoradi611.mm@gmail.com>
2022-04-27 13:55:42 +0200

    AVrage function

--- javascript/function/js/main.js
+++ javascript/function/js/main.js
@@ -1,69 +1,94 @@
 // function declaration
 // function expression
 // ************************************************  Function
-// ........................  
+// ........................
 
-console.log('First name : Mahsa');
-console.log('Last name : Moradi');
-console.log('Age : 29');
+console.log("First name : Mahsa");
+console.log("Last name : Moradi");
+console.log("Age : 29");
 // ...................
-var a=['First name : Mahsa','Last name : Moradi','Age : 29']
-for(i=0;i<=addEventListener.length;i++){
-
-}
+var a = ["First name : Mahsa", "Last name : Moradi", "Age : 29"];
+for (i = 0; i <= addEventListener.length; i++) {}
 console.log(a);
 // ............................. ********* function declaration
 // 1a ........................
 // inputs (num1,num2) === parameter
-function functionName(inputs){
-    // code
-
+function functionName(inputs) {
+  // code
 }
 // functionName(); === argument
 functionName();
 // 2a ........................
-function showBio(){
-    console.log('First name : ********* Mahsa');
-    console.log('Last name : Moradi');
-    console.log('Age : 29');
-
+function showBio() {
+  console.log("First name : ********* Mahsa");
+  console.log("Last name : Moradi");
+  console.log("Age : 29");
 }
 showBio();
 showBio();
 showBio();
 // 3a ........................ add 2 number
-function addnumbers(num1 , num2){
-    console.log(num1 + num2);
+function addnumbers(num1, num2) {
+  console.log(num1 + num2);
 }
-addnumbers(5,200);
-addnumbers(5,10);
+addnumbers(5, 200);
+addnumbers(5, 10);
 // ________
-function addnumbers1(num1 , num2){
-    console.log(num1 + num2);
+function addnumbers1(num1, num2) {
+  console.log(num1 + num2);
 }
-var a=addnumbers1(62,400);
+var a = addnumbers1(62, 400);
 console.log(a);
 // ===
-function addnumbers2(num1 , num2){
-    return(num1 + num2);
+function addnumbers2(num1, num2) {
+  return num1 + num2;
+  // dastor bad return Ejra nemisheeeeeh  (not run)
+  console.log(b);
 }
-var b=addnumbers2(62,400);
+var b = addnumbers2(62, 400);
 console.log(b);
+// 4a ............................ AVrage
+var c1 = 12;
+var c2 = 20;
+var c3 = 14;
+var c4 = 19;
+var c5 = 10;
+var sum = c1 + c2 + c3 + c4 + c5;
+var av = sum / 5;
+// console.log(av);
+if (av >= 15) {
+  console.log("good");
+} else {
+  console.log("not good");
+}
+// ______________
+function e(c1, c2, c3, c4, c5) {
+  sum = c1 + c2 + c3 + c4 + c5;
+  av = sum / 5;
+  if (av >= 15) {
+    console.log("good");
+  } else {
+    console.log("not good");
+  }
+}
+e(15, 10, 2, 20, 19);
+// var f=e(15, 10, 2, 20, 19);
+// console.log(f);
 // ............................. ********* function expression
 // 1b ........................
-var name = function(){
-    // code
-}
+var name = function () {
+  // code
+};
 // 2b ........................
-var names = function(){
-    console.log('First name : ....... Mehrdad');
-    console.log('Last name : Moradi');
-    console.log('Age : 34');
-}
+var names = function () {
+  console.log("First name : ....... Mehrdad");
+  console.log("Last name : Moradi");
+  console.log("Age : 34");
+};
 names();
 // 3b ........................ add 2 number
-var numbers1=function(num1,num2){
-    console.log(num1+num2);
-}
-numbers1(50,14);
+var numbers1 = function (num1, num2) {
+  console.log(num1 + num2);
+};
+numbers1(50, 14);
 // OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
