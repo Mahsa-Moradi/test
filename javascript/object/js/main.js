cf3b8f0f76e4922ad5e83441c893f1b52828c64e
mahsa <mahsamoradi611.mm@gmail.com>
2022-05-03 00:21:24 +0200

    object empty

--- javascript/object/js/main.js
+++ javascript/object/js/main.js
@@ -12,23 +12,40 @@ const object_name1 = {
   firstname: "mahsa",
   lastname: "moradi",
   age: "36",
-  favorites: ["painting", "music"]
+  favorites: ["painting", "music"],
 };
 console.log(object_name1.favorites);
 // 2 ......................
 const object_name2 = {
-    firstname: "mahsa",
-    lastname: "moradi",
-    age: "36",
-    favorites: ["painting", "music"]
-  };
-  console.log(object_name2['age']);
+  firstname: "mahsa",
+  lastname: "moradi",
+  age: "36",
+  favorites: ["painting", "music"],
+};
+console.log(object_name2["age"]);
 // ************************************** object method
 const object_method = {
-    firstname :'mehrdad',
-    lastname :'moradi',
-    codeing : function(){
-return 'mahsa is coding ... ';
-    }
-}
+  firstname: "mehrdad",
+  lastname: "moradi",
+  codeing: function () {
+    return "mahsa is coding ... ";
+  },
+};
 console.log(object_method.codeing());
+// ************************************** object nesting
+const sara = {
+  firstname: "sara",
+  lastname: "loren",
+  age: "29",
+  codeing : function(){
+return 'Hello sara ... '
+  },
+  grates: {
+    Math: 20,
+    arabic: 18,
+    sport: 20,
+    geometry: 16,
+  }
+}
+console.log(sara);
+console.log(sara.grates.geometry);
