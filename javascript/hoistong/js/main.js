0bd1fa9fff66a700143115cfccbd2914dead8db8
mahsa <mahsamoradi611.mm@gmail.com>
2022-05-02 13:48:19 +0200

    let , const != hoisting

--- javascript/hoistong/js/main.js
+++ javascript/hoistong/js/main.js
@@ -1,11 +1,32 @@
-// ***************************  Hoisting
-// declare (tarif kardan) _____________ var a;
-// initialize (meghdar dehi kardan) ___ a=5; 
+// ***************************(let ,const)!= Hoisting = var
+// declare (tarif kardan) _____________ var a / var firstName;
+// initialize (meghdar dehi kardan) ___ a=5 / firstName ='mahsa';; 
+var firstName='mahsa';
+console.log(firstName);
+// ============
+// declare = hoistong
+var firstName;
+console.log(firstName);
+firstName ='mahsa';
+// initialize != hoisting
+// ............................ let , const != hoisting
+let firstName1;
+console.log(firstName1);
+firstName1 ='mahsa';
+// .......................
+// const firstName2;
+// console.log(firstName2);
+// firstName2 = 'moradi';
 
+// ...................................
+const a=5;
+function num(){
+    b=6;
+    console.log(b);
+    var b;
+    }
 
-
-
-
+num();
 
 
 
