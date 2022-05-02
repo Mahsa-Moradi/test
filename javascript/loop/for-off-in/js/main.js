45b906417520d25beb1beea84ef418a00d6f193e
mahsa <mahsamoradi611.mm@gmail.com>
2022-05-03 01:56:46 +0200

    for-in

--- javascript/loop/for-off-in/js/main.js
+++ javascript/loop/for-off-in/js/main.js
@@ -30,7 +30,7 @@ console.log('********************** for in');
 // for(key in object){
 
 // }
-// ......................
+// ......................dastre30 be key
 const myArray1 = [25,58,74,95,65,23,145];
 const mahsa = {
   firstname : 'mahsa',
@@ -40,4 +40,19 @@ const mahsa = {
 };
 for(let key in mahsa){
   console.log(key);
+}
+//.......................
+console.log('..................');
+// .......................... dastre30 be value
+const myArray2 = [25,58,74,95,65,23,145];
+const mimi = {
+  firstname : 'mimi',
+  lastname : 'moradi',
+  age : '29',
+
+};
+for(let key in mimi){
+  // console.log(mimi.firstname);
+  // console.log(mimi[key]);
+  console.log(key + " ==> " + mimi[key] );
 }
