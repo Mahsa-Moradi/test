7c8beb522214038bca7e8fa9d9c3f09837b9f977
mahsa <mahsamoradi611.mm@gmail.com>
2022-05-03 01:38:04 +0200

    for-off

--- javascript/loop/for-off-in/js/main.js
+++ javascript/loop/for-off-in/js/main.js
@@ -1,18 +1,25 @@
 // ************************************************** for-off
-// array / string / map / set(majmoeha)
+//      [  array / string / map / set(majmoeha)  ]
 
-for(let element of itrabel){
-  console.log();
-}
+// for(let element of itrabel){
+//   console.log(element);
+// }
+
 // ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
 let myArray=[15,47,87,95,36,25,12];
 for(i=0 ; i<=myArray.length ; i++){
   console.log(myArray[i]);
 }
 // ______________
-console.log('oooooooooooooooooooooooooooooooooo');
-// _____________ for-off
+console.log('**********************');
+// _____________ for-off ____________ element ya har esme digeii
 for(let element of myArray){
   console.log(element);
 }
-// _________________
+//.......................
+console.log('**********************');
+// ......................
+let str = 'mahsa moradi';
+for(element of str){
+  console.log(element);
+}
