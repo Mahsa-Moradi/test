d43ec3b52d3f05302c26504256fa2fdb626567cc
mahsa <mahsamoradi611.mm@gmail.com>
2022-05-06 15:18:47 +0200

    end accordion-menu

--- JQuery/collaps/js/main.js
+++ JQuery/collaps/js/main.js
@@ -1,10 +1,25 @@
-$(document).ready(function () {
-  $(".collpse-heading").click(function () {
-    $(".collapse-controls").removeClass("open");
-    if ($(this).next(".collapse-body").css("display") == "block") {
-      $(".collapse-body").slideUp("open");
-    } else {
-      $(".collapse-body").slideDown("open");
-    }
-  });
+// $(document).ready(function () {
+//   $(".collpse-heading").click(function () {
+//     $(".collapse-controls").removeClass("open");
+//     if ($(this).next(".collapse-body").css("display")=="block") {
+//       $(".collapse-body").slideUp("open");
+//     } else {
+//         $(this).next(".collapse-body").slideDown();
+//       $(".collapse-body").slideDown("open");
+//     }
+//   });
+// });
+// ............................................
+$(document).ready(function(){
+$('.collpse-heading').click(function(){
+$('.collapse-controls').removeClass('.open');
+if($(this).next('.collapse-body').css('display')== 'block'){
+    $('.collapse-body').slipeUp('open');
+}else{
+    $(this).next('.collapse-body').slipeDown();
+    $(this).children('.collapse-controls').addClass('open');
+}
+
+
+});
 });
