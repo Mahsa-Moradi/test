aa23e0828f3eeff1f801f5fca94cd59795203184
mahsa <mahsamoradi611.mm@gmail.com>
2022-05-06 13:56:49 +0200

    js

--- JQuery/collaps/js/main.js
+++ JQuery/collaps/js/main.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
-  $('.collpse-heading').click(function(){
-     $('.collapse-controls').removeclass('.open')
-
-  })  
-})
+$(document).ready(function () {
+  $(".collpse-heading").click(function () {
+    $(".collapse-controls").removeClass("open");
+    if ($(this).next(".collapse-body").css("display") == "block") {
+      $(".collapse-body").slideUp("open");
+    } else {
+      $(".collapse-body").slideDown("open");
+    }
+  });
+});
