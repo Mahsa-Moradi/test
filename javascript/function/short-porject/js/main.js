2e884703c95557c7b25e4ad634b3690019f8588b
mahsa <mahsamoradi611.mm@gmail.com>
2022-05-10 13:38:31 +0200

    js

--- javascript/function/short-porject/js/main.js
+++ javascript/function/short-porject/js/main.js
@@ -4,13 +4,13 @@ const quotes = [
   "item 3",
   "item 4",
   "item 5",
-  "item 6",
-  "item 6",
-];
-const paragraphQuotes = document.getElementById("quote")
-  function showNewQuote() {
-    // alert("hellooooooooooo");
-    // const user = prompt('enter a number :')
-    const randomNumber =Math.floor( Math.random()*quotes.lenght)
-    paragraphQuotes.innerText =quotes[0]
-  };
+  "item 6"
+]
+const paragraphQuotes = document.getElementById("quote");
+
+
+function showNewQuote() {
+  
+  const randomNumber = Math.floor(Math.random() * quotes.lenght)
+  paragraphQuotes.innerText = quotes[randomNumber]
+}
