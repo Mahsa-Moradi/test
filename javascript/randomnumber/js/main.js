e4550dc657ed1eaf2b16973b5fb9acd7c03d978a
mahsa <mahsamoradi611.mm@gmail.com>
2022-05-10 12:02:43 +0200

    Random_Number

--- javascript/randomnumber/js/main.js
+++ javascript/randomnumber/js/main.js
@@ -37,20 +37,18 @@ console.log(randomNumber2);
 // }
 
 //  .................................
-let password = 'mahsa123';
-let i = 1
-while(i <= 3){
-    const user = prompt('Enter your password :')
-    if(user == password){
-        alert('succes ... ');
-        break;
-
-    }else{
-        const massage = prompt('Try again  ${3-i}')
-        alert('Try again ...');
-    }
-    i++;
-
+let password = "mahsa123";
+let i = 1;
+while (i <= 3) {
+  const user = prompt("Enter your password :");
+  if (user == password) {
+    alert("succes ... ");
+    break;
+  } else {
+    // const massage = prompt()
+    alert(`Try again ${3 - i} times`);
+  }
+  i++;
 }
 // .................................
 
