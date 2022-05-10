6a6b2d68fd48dc8f501a4a47c422c798eb78b350
mahsa <mahsamoradi611.mm@gmail.com>
2022-05-10 13:27:57 +0200

    js

--- javascript/function/short-porject/js/main.js
+++ javascript/function/short-porject/js/main.js
@@ -1,2 +1,16 @@
-const quotes = ['item 1','item 2','item 3','item 4','item 5','item 6','item 6']
-const paragraphQuotes = document.getElementById('quote').innerHTML=
+const quotes = [
+  "item 1",
+  "item 2",
+  "item 3",
+  "item 4",
+  "item 5",
+  "item 6",
+  "item 6",
+];
+const paragraphQuotes = document.getElementById("quote")
+  function showNewQuote() {
+    // alert("hellooooooooooo");
+    // const user = prompt('enter a number :')
+    const randomNumber =Math.floor( Math.random()*quotes.lenght)
+    paragraphQuotes.innerText =quotes[0]
+  };
