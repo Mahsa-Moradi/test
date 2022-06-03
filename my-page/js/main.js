98acff5e6a8692c0e4d95453fd48b110e602cb77
mahsa <mahsamoradi611.mm@gmail.com>
2022-06-03 17:55:39 +0200

    my-page

---
+++ my-page/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#drawer-toggle-label").click(function () {
+    $("ul #drawer").css({
+      // 'position': 'absolute',
+      'left': '0',
+    });
+  });
+});
