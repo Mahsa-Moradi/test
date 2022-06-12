6aed26ce65c9939190974f92c9e5b62af888b1ee
mahsa <mahsamoradi611.mm@gmail.com>
2022-06-12 20:14:05 +0200

    js

---
+++ my-page/js/main.js
@@ -0,0 +1,9 @@
+let point=document.querySelector(".pointer");
+document.addEventListener("mousemove",function(e){
+point.getElementsByClassName.left=e.pageX + "px";
+point.getElementsByClassName.top=e.pageY + "px";
+
+})
+
+
+ 
